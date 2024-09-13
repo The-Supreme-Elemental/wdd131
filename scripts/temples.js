e2f7954444baf5031dd5f0ef803fee15ff4f986b
Christopher Hylton <jamboyh5@gmail.com>
2024-09-13 17:07:38 -0600

    templesssss

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,7 @@
 const mainnav = document.querySelector(".navigation")
 const hambutton = document.querySelector("#menu");
 
-hambutton.addEventListener.toggle("click", () => {
+hambutton.addEventListener("click", () => {
     mainnav.classList.toggle("show");
     hambutton.classList.toggle("show");
 });
