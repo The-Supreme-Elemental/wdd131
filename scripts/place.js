af187a915c4f755a7e6d210b095be64f116a3fc5
Christopher Hylton <jamboyh5@gmail.com>
2024-10-08 22:13:06 -0600

    make windchill  function display on the document

--- scripts/place.js
+++ scripts/place.js
@@ -6,6 +6,13 @@ let lastModifiedDate = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = "Last Modified:  " + lastModifiedDate;
 
 function calcWindChill(airTemp, windSpeed) {
-    console.log(35.74 + (0.6215 * airTemp) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * airTemp * (windSpeed ** 0.16)))
+
+    if (airTemp <= 50 && windSpeed > 3) {
+        return 35.74 + (0.6215 * airTemp) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * airTemp * (windSpeed ** 0.16));
+    } else {
+        return "N/A";
+    }
 }
 
+let windChill = calcWindChill(40, 10);
+document.querySelector("windChill").textContent = windChill;
