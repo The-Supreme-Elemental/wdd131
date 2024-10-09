382c69078ea77740ecc6b7863bbc6267c6c8d377
Christopher Hylton <jamboyh5@gmail.com>
2024-10-08 21:51:29 -0600

    place javascript

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,8 @@ document.getElementById("current_year").innerHTML = current_year.getFullYear();
 let lastModifiedDate = new Date(document.lastModified);
 
 document.getElementById("lastModified").innerHTML = "Last Modified:  " + lastModifiedDate;
+
+function calcWindChill(airTemp, windSpeed) {
+    console.log(35.74 + (0.6215 * airTemp) - (35.75 * (windSpeed ** 0.16)) + (0.4275 * airTemp * (windSpeed ** 0.16)))
+}
+
